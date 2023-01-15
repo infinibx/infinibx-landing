4480946f575c743732972842df04263644e1fbc8
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-01-15 16:00:05 +0100

    created newsletter, update css

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,23 @@
+class NewslettersController < ApplicationController
+  # skip_before_action :authenticate_user!
+  before_action :set_newsletter, only: [:destroy]
+
+  def create
+    @newsletter = Newsletter.create(newsletter_params)
+    if @newsletter.save
+      redirect_to blog_index_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_newsletter
+    @newsletter = Newsletter.find(params[:id])
+  end
+
+  def newsletter_params
+    params.require(:newsletter).permit(:name, :email)
+  end
+end
