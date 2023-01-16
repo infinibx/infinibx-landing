583055bdfb1ac6608ef0089a8fdaaf3b95f120e0
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-01-16 23:21:14 +0100

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'newsletters/create'
+  get 'about', to: 'pages#about'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
